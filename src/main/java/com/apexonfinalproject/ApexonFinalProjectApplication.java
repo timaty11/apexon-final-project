784d1b273d85e23e4131944e877792bbc5be7bbd
Tymofeii <tima130603@gmail.com>
2022-12-07 07:13:59 -0300

    initial commit

---
+++ src/main/java/com/apexonfinalproject/ApexonFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.apexonfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApexonFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApexonFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apexonfinalproject/ApexonFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apexonfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexonFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
